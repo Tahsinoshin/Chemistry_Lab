3bee72d4c0e362b8515d8abddcc187fcf5c9c111
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2022-12-12 03:35:44 +0600

    third commit

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,17 +4,25 @@ import "./Feed.css";
 export default function Feed() {
   return (
     <div className="wrapper">
-        <div classname="container">
+      <div classname="container">
         <div className="about-feed">
-            <div className="about-description">
-                <h1>about description</h1>
+            <div className="description">
+              <h1>About the Lab</h1>
+              <div className="about-des">
+              <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.May 20, 2020</p>
+              </div> 
             </div>
-            <div className="about-img">
-                <h1>about img</h1>
-                <img src="assets/image1.jpg" alt=" description" />
+            <div className="des-img">
+              <img id="image1" src="/assets/image1.jpg" alt="img1" />
             </div>
         </div>
-    </div>
+        <div className="contact">
+          <div className="info">
+            <h1>contact info</h1>
+            <p>The lorem ipsum is a placeholder text used in publishing</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
