98ba2eda835c4bfebad6a9135ab3942e7baa6ffe
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2022-12-28 01:47:11 +0600

    rough commit

--- src/pages/people/faculty/Faculty.jsx
+++ src/pages/people/faculty/Faculty.jsx
@@ -1,45 +1,53 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { Col, Row } from 'antd';
 import './Faculty.css'
 import Topbar from '../../../components/topbar/Topbar';
+import individualFaculty from './IndividualFaculty'
+import IndividualFaculty from './IndividualFaculty';
 
 const studentCategories = [
   {
     key: '1',
     image: '/assets/faculty1.jpg',
     title: 'faculty 1',
-    detail: 'taking course on natural resource project'
+    detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    publication: 'https://www.nature.com/articles/s41467-021-21459-y'
     
   },
   {
     key: '2',
     image: '/assets/faculty2.jpg',
     title:  'faculty 2',
-    detail: 'taking course on natural resource project'
+    detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    publication: 'https://www.nature.com/articles/s41467-021-21459-y'
   },
   {
     key: '3',
     image: '/assets/faculty3.jpg',
     title:  'faculty 3',
-    detail: 'taking course on natural resource project'
+    detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    publication: 'https://www.nature.com/articles/s41467-021-21459-y'
   },
   {
     key: '4',
     image:'/assets/faculty4.jpg',
     title:  'faculty 4',
-    detail: 'taking course on natural resource project'
+    detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    publication: 'https://www.nature.com/articles/s41467-021-21459-y'
   },
   {
     key: '5',
     image: '/assets/faculty5.jpg',
     title:  'faculty 5',
-    detail: 'taking course on natural resource project'
+    detail:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    publication: 'https://www.nature.com/articles/s41467-021-21459-y'
   },
   {
     key: '6',
     image: '/assets/faculty6.jpg',
     title:  'faculty 6',
-    detail: 'taking course on natural resource project'
+    detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    publication: 'https://www.nature.com/articles/s41467-021-21459-y'
   }
 ]
 
@@ -47,7 +55,17 @@ function showStudent(){
   studentCategories.map(studentCategory =>{console.log(studentCategory.image)});
 }
 
-export default function UnderGraduate() {
+
+
+export default function Faculty() {
+
+  let [showMore, setshowMore]= useState(false);
+
+  function handleShow(){
+
+    setshowMore(true); 
+  
+  } 
   return (
     <>
     
@@ -57,20 +75,24 @@ export default function UnderGraduate() {
       <h2>Allocated Faculties</h2>
         <div className="grids">
           <Row gutter={[24, 24]}>
-            {studentCategories.map(studentCategory => {
+            {studentCategories.map((studentCategory, i) => {
+              let text= studentCategory.detail+studentCategory.publication
+              let showText= text.slice(0,100)
               return (
                 <Col xs={{ span: 8 }} sm={{ span: 6 }} lg={{ span: 8 }} key={studentCategory.key}>
-                <div className="content">
+                <div className="content" key={i}>
                   <div className="image">
                     <img src={studentCategory.image} alt="student" />
                   </div>
                   <h3>{studentCategory.title}</h3>
-                  <h5>{studentCategory.detail}</h5>
+                  <h5>{showText}...</h5>
+                  <button className="btn btn-primary"  onClick={handleShow}>View Details</button>
                 </div>
                 </Col>
               );
             })}
           </Row>
+          <div>{showMore && <IndividualFaculty />}</div>
         </div>
       </div>
     </>
---
+++ src/pages/people/faculty/IndividualFaculty.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Faculty from './Faculty'
+
+export default function IndividualFaculty({props}) {
+  return (
+    <div>
+      <h1>{props.detail}</h1>
+    </div>
+  )
+}
