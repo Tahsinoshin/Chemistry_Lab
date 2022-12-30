8ef83d55f38cfe3f18e502b350e6c82aa2f96d8c
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2022-12-30 22:05:44 +0600

    eleventh commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,46 +3,45 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          {/* Column1 */}
-          <div className="col">
-            <h4>THICC MEMES INC</h4>
-            <h1 className="list-unstyled">
-              <li>342-420-6969</li>
-              <li>Moscow, Russia</li>
-              <li>123 Streeet South North</li>
-            </h1>
-          </div>
-          {/* Column2 */}
-          <div className="col">
-            <h4>Stuff</h4>
-            <ui className="list-unstyled">
-              <li>DANK MEMES</li>
-              <li>OTHER STUFF</li>
-              <li>GUD STUFF</li>
-            </ui>
-          </div>
-          {/* Column3 */}
-          <div className="col">
-            <h4>WELL ANOTHER COLUMN</h4>
-            <ui className="list-unstyled">
-              <li>DANK MEMES</li>
-              <li>OTHER STUFF</li>
-              <li>GUD STUFF</li>
-            </ui>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} THICC MEMES | All rights reserved |
-            Terms Of Service | Privacy
+    <footer className="footer-distributed">
+      <div className="footer-left">
+          <h3>Chemistry<span>Lab</span></h3>
+          <p className="footer-links">
+            Department of Chemistry, University of Dhaka
           </p>
-        </div>
+          <p className="footer-company-name">Copyright © 2021 <strong>ChemistryLab</strong> All rights reserved</p>
       </div>
-    </div>
+
+      <div className="footer-center">
+          <div>
+              <i className="fa fa-map-marker"></i>
+              <p><span>Curzon Hall</span>
+                  University of Dhaka</p>
+          </div>
+
+          <div>
+              <i className="fa fa-phone"></i>
+              <p>+91 74**9**258</p>
+          </div>
+          <div>
+              <i className="fa fa-envelope"></i>
+              <p><a href="mailto:sagar00001.co@gmail.com">xyz@gmail.com</a></p>
+          </div>
+      </div>
+      <div className="footer-right">
+          <p className="footer-company-about">
+              <span>About the Developers</span>
+              This website is Developed by <a href="/">Md. Muktar Hsen Mrihda</a> and <a href="/">Samiha Tahsin Noshin</a>
+          </p>
+          <div className="footer-icons">
+              <a href="#"><i className="fa fa-facebook"></i></a>
+              <a href="#"><i className="fa fa-instagram"></i></a>
+              <a href="#"><i className="fa fa-linkedin"></i></a>
+              <a href="#"><i className="fa fa-twitter"></i></a>
+              <a href="#"><i className="fa fa-youtube"></i></a>
+          </div>
+        </div>
+      </footer>
   );
 }
 
