f4778ed381ce83a13cabcfbe0fe8db1a36b4d932
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2023-01-14 02:21:28 +0600

    13th commit

--- src/pages/publication/book/Book.jsx
+++ src/pages/publication/book/Book.jsx
@@ -1,48 +1,106 @@
-import React from "react";
-import './Book.css';
-import Searchbar from "../searchBar/Searchbar";
-import Topbar from "../../../components/topbar/Topbar";
-import {list} from "../List"
-
-
+import React, { useState, useEffect } from "react";
+import "./Book.css";
+import axios from "axios";
 
 export default function Book() {
-  return (
-    <>
-      {/* <Topbar /> */}
-      <Searchbar data={list} />
-      <section className="book-main">
-        <div className="book-container">
-          <h1>Published Books</h1>
-          <div class="d-flex justify-content-center">
-            <table class="table table-striped table-hover">
-              <thead>
-                <tr>
-                  <th scope="col">id</th>
-                  <th scope="col">Author</th>
-                  <th scope="col">Publication</th>
-                  <th scope="col">Year</th>
-                </tr>
-              </thead>
+  console.log("running");
+  const [data, setData] = useState([]);
+  const [filterVal, setFilterVal] = useState("");
+  const [searchApiData, setSearchApiData] = useState("");
 
-              <tbody>
-                {list.map((li) => {
+  useEffect(() => {
+    const getData = () => {
+      axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+        setData(res.data);
+        console.log(res.data);
+        setSearchApiData(res.data);
+      });
+    };
+    getData();
+  }, []);
+
+  const handleFilter = (e) => {
+    if (e.target.value === "") {
+      setData(searchApiData);
+    } else {
+      const filterResult = searchApiData.filter((item) =>
+        item.name.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      setData(filterResult);
+    }
+    setFilterVal(e.target.value);
+  };
+  return (
+    <div className="justify-content-center main-div">
+      <div className="shadow-sm p-3 mb-5 bg-body rounded book-container ">
+        <div className="input-box">
+          <input
+            class="fa fa-search"
+            type="text"
+            placeholder="search"
+            value={filterVal}
+            onInput={(e) => {
+              handleFilter(e);
+            }}
+          />
+        </div>
+        <h1>Published Books</h1>
+        <div class="table-responsive-md">
+          <table class="table table-hover table-striped">
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>usename</th>
+              </tr>
+              {data &&
+                data.map((item) => {
                   return (
                     <tr>
-                      {/* <th scope="row">1</th> */}
-                      <td>{li.key}</td>
-                      <td>{li.author}</td>
-                      <td><a href={li.publication}>{li.publication}</a></td>
-                      <td>{li.year}</td>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.username}</td>
                     </tr>
                   );
                 })}
-              </tbody>
-            </table>
-          </div>
+            </tbody>
+          </table>
         </div>
-      </section>
-      
-    </>
+      </div>
+    </div>
   );
 }
+
+// {/* <Topbar /> */}
+// <Searchbar data={list} />
+// <section className="book-main">
+//   <div className="book-container">
+//     <h1>Published Books</h1>
+//     <div class="d-flex justify-content-center">
+//       <table class="table table-striped table-hover">
+//         <thead>
+//           <tr>
+//             <th scope="col">id</th>
+//             <th scope="col">Author</th>
+//             <th scope="col">Publication</th>
+//             <th scope="col">Year</th>
+//           </tr>
+//         </thead>
+
+//         <tbody>
+//           {list.map((li) => {
+//             return (
+//               <tr>
+//                 {/* <th scope="row">1</th> */}
+//                 <td>{li.key}</td>
+//                 <td>{li.author}</td>
+//                 <td><a href={li.publication}>{li.publication}</a></td>
+//                 <td>{li.year}</td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   </div>
+// </section>
--- src/pages/publication/conference/Conference.jsx
+++ src/pages/publication/conference/Conference.jsx
@@ -1,45 +1,106 @@
-import React from "react";
-import './Conference.css';
-import Searchbar from "../searchBar/Searchbar";
-import Topbar from "../../../components/topbar/Topbar";
-import {list} from "../List"
+import React, { useState, useEffect } from "react";
+import "./Conference.css"; 
+import axios from "axios";
 
 export default function Conference() {
-  return (
-    <>
-      <Topbar />
-      <Searchbar />
-      <section className="main">
-        <div className="container">
-          <h1>Published Conference Paper</h1>
-          <div className="element">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th scope="col">id</th>
-                  <th scope="col">Author</th>
-                  <th scope="col">Publication</th>
-                  <th scope="col">Year</th>
-                </tr>
-              </thead>
+  console.log("running");
+  const [data, setData] = useState([]);
+  const [filterVal, setFilterVal] = useState("");
+  const [searchApiData, setSearchApiData] = useState("");
 
-              <tbody>
-                {list.map((li) => {
+  useEffect(() => {
+    const getData = () => {
+      axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+        setData(res.data);
+        console.log(res.data);
+        setSearchApiData(res.data);
+      });
+    };
+    getData();
+  }, []);
+
+  const handleFilter = (e) => {
+    if (e.target.value === "") {
+      setData(searchApiData);
+    } else {
+      const filterResult = searchApiData.filter((item) =>
+        item.name.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      setData(filterResult);
+    }
+    setFilterVal(e.target.value);
+  };
+  return (
+    <div className="justify-content-center">
+      <div className="shadow-sm p-3 mb-5 bg-body rounded book-container ">
+        <div className="input-box">
+          <input
+            class="glyphicon glyphicon-search"
+            type="text"
+            placeholder="search"
+            value={filterVal}
+            onInput={(e) => {
+              handleFilter(e);
+            }}
+          />
+        </div>
+        <h1>Conference Papers</h1>
+        <div class="table-responsive-md">
+          <table class="table table-hover">
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>usename</th>
+              </tr>
+              {data &&
+                data.map((item) => {
                   return (
                     <tr>
-                      {/* <th scope="row">1</th> */}
-                      <td>{li.key}</td>
-                      <td>{li.author}</td>
-                      <td><a href={li.publication}>{li.publication}</a></td>
-                      <td>{li.year}</td>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.username}</td>
                     </tr>
                   );
                 })}
-              </tbody>
-            </table>
-          </div>
+            </tbody>
+          </table>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 }
+
+// {/* <Topbar /> */}
+// <Searchbar data={list} />
+// <section className="book-main">
+//   <div className="book-container">
+//     <h1>Published Books</h1>
+//     <div class="d-flex justify-content-center">
+//       <table class="table table-striped table-hover">
+//         <thead>
+//           <tr>
+//             <th scope="col">id</th>
+//             <th scope="col">Author</th>
+//             <th scope="col">Publication</th>
+//             <th scope="col">Year</th>
+//           </tr>
+//         </thead>
+
+//         <tbody>
+//           {list.map((li) => {
+//             return (
+//               <tr>
+//                 {/* <th scope="row">1</th> */}
+//                 <td>{li.key}</td>
+//                 <td>{li.author}</td>
+//                 <td><a href={li.publication}>{li.publication}</a></td>
+//                 <td>{li.year}</td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   </div>
+// </section>
